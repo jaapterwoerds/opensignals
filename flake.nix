c24989692e3eca75961837cf6891bc94322c1c18
Jaap ter Woerds <jaap@lambdalab.nl>
2022-09-17 16:20:01 +0200

    WIP

--- flake.nix
+++ flake.nix
@@ -13,15 +13,8 @@
     {
       # enter this python environment by executing `nix shell .`
       defaultPackage = forAllSystems (system: pkgs: mach-nix.lib."${system}".mkPython {
-        requirements = ''
-            docopt>=0.6.2
-            pandas>=1.2.2
-            numpy>=1.21.4
-            pyarrow>=6.0.1
-            requests>=2.25.1
-            tqdm>=4.61.2
-        '';
-         packagesExtra =builtins.readFile ./requirements-dev.txt;
+        requirements = builtins.readFile ./requirements.txt;
+        #  packagesExtra =builtins.readFile ./requirements-dev.txt;
       });
     };
 }
